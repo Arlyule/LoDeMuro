f176da27e5c96a450ae2d697a4237fc9e3cc7289
Papatoncio <juanpij23@hotmail.com>
2023-11-29 18:41:19 -0600

    comi

---
+++ src/app/pages/nosotros/nosotros.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nosotros',
+  templateUrl: './nosotros.component.html',
+  styleUrls: ['./nosotros.component.css']
+})
+export class NosotrosComponent {
+  nosotros = [];
+  // Aquí puedes definir los valores que quieres mostrar en el componente
+  valores = [
+    { nombre: 'Rendición de Cuentas: ', descripcion: 'Ejercemos nuestras funciones en el marco de las leyes, reglamentos y normas que las rigen.' },
+    { nombre: 'Transparencia: ', descripcion: 'Brindamos acceso a la toda la información pública que se genera o posea la universidad.' },
+    { nombre: 'Honestidad: ', descripcion: 'Ofrecemos lo que podemos cumplir y nos esmeramos en lograrlo.' },
+    { nombre: 'Responsabilidad: ', descripcion: 'Ponemos atención especial en nuestras decisiones.' },
+    { nombre: 'Respeto: ', descripcion: 'Tratamos a las personas con dignidad y apreciamos sus opiniones.' },
+    { nombre: 'Paz: ', descripcion: 'Rechazamos cualquier tipo de violencia. Practicamos la empatía y asertividad. Somos solidarios (as) con los demás. Respetamos las opiniones de los demás.' }
+  ];
+  mision = ["Educando al Futuro de México  |  Somos una institución de Educación Superior Tecnológica comprometida en la formación integral de profesionistas líderes en los niveles de Técnico Superior Universitario y Licenciatura con programas educativos de excelencia, contribuyendo así, al desarrollo económico y social del estado y la región."]
+}
